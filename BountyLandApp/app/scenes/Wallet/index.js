67a1d4cee9a1b0ddeff54ef45d61572427840106
Jiri Spac <capajj@gmail.com>
2017-04-07 21:50:10 +0200

    added two tabs, standard, watchman

--- BountyLandApp/app/scenes/Wallet/index.js
+++ BountyLandApp/app/scenes/Wallet/index.js
@@ -2,22 +2,29 @@ import React, { Component } from 'react'
 import { View, Platform } from 'react-native'
 import * as firebase from 'firebase'
 import Toast from 'react-native-toast'
-import { Container, Content, Card, CardItem, Text, Body, H2, Button,  Row, Form, Item, Label, Input } from 'native-base'
+import { Header, Container, Content, Card, CardItem, Text, Body, H2, Button, Row, Form, Item, Label, Input, Tabs, Tab, TabHeading, Icon } from 'native-base'
+import { WebView } from 'react-native'
+//import checkout from '../../checkout.html'
 
+const a = 19922
 class Wallet extends Component {
-    render () {
-        return (
-            <View>
-                
-            </View>
-        )
-    }
+  render () {
+    return (
+      <Container>
+        <Header hasTabs  />
+        <Tabs>
+          <Tab heading={ <TabHeading><Icon name="camera" /><Text>Transactions</Text></TabHeading>}>
+            <Text>Balance</Text>
+            <H2>{a}</H2>
+          </Tab>
+          <Tab heading={ <TabHeading><Icon name="import export" /><Text>Top up</Text></TabHeading>}>
+            <Text>Top up</Text>
+            <Input></Input>
+          </Tab>
+        </Tabs>
+      </Container>
+    )
+  }
 }
 
 export default Wallet
-
-
-
-
-
-
