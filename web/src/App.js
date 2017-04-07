540c6779b657c855e8567093ccc16dbed0a9178a
Jakub Benes <jukben@gmail.com>
2017-04-08 01:42:02 +0200

    drawer menu

--- web/src/App.js
+++ web/src/App.js
@@ -5,12 +5,24 @@ import {
   Link,
   Redirect
 } from 'react-router-dom'
+import './App.scss'
 import * as Routes from './routes'
+import { observer } from 'mobx-react'
+import { observable } from 'mobx'
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
 import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
 import getMuiTheme from 'material-ui/styles/getMuiTheme'
-import { auth, storageKey, isAuthenticated, login, logout } from './firebase'
+import { Drawer, MenuItem, AppBar } from 'material-ui'
+import { auth, isAuthenticated, login, logout } from './firebase'
+import injectTapEventPlugin from 'react-tap-event-plugin'
 
+injectTapEventPlugin()
+
+const AppStore = observable({
+  menuIsOpen: false
+})
+
+@observer
 class App extends React.Component {
   componentDidMount () {
     auth.onAuthStateChanged(user => {
@@ -24,36 +36,50 @@ class App extends React.Component {
     })
   }
 
+  toggleMenu = () => {
+    AppStore.menuIsOpen = !AppStore.menuIsOpen
+  }
+
   processLogout = () => {
     this.setState({logged: false})
+    this.toggleMenu()
     logout()
   }
 
   render () {
     return (
       <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
-        <Router>
-          <div>
-            <ul>
-              <li><Link to='/'>Home</Link></li>
-              <li><Link to='/about'>About</Link></li>
-              <ProtectedLink to='/profile'>Profile</ProtectedLink>
-              <ProtectedLink to='/wallet'>Wallet</ProtectedLink>
-              {!isAuthenticated()
-                ? <li><Link to='/login'>Login</Link></li>
-                : <li onClick={logout}>Logout</li>
-              }
-            </ul>
+        <div>
+          <AppBar
+            title="Title"
+            iconClassNameRight="muidocs-icon-navigation-expand-more"
+            onLeftIconButtonTouchTap={this.toggleMenu}
+          />
+          <Router>
+            <div>
+              <Drawer
+                docked={false}
+                width={200}
+                open={AppStore.menuIsOpen}
+                onRequestChange={(open) => AppStore.menuIsOpen = open}
+              >
+                <MenuItem onTouchTap={this.toggleMenu}><Link to='/'>Home</Link></MenuItem>
+                <ProtectedLink to='/profile'>Profile</ProtectedLink>
+                <ProtectedLink to='/wallet'>Wallet</ProtectedLink>
+                {!isAuthenticated()
+                  ? <MenuItem onTouchTap={this.toggleMenu}><Link to='/login'>Login</Link></MenuItem>
+                  : <MenuItem onTouchTap={this.processLogout}>Logout</MenuItem>
+                }
+              </Drawer>
 
-            <hr />
-
-            <Route exact path='/' component={Routes.Home} />
-            <Route exact path='/profile' component={Routes.Profile} />
-            <Route exact path='/login' component={Routes.Login} />
-            <Route path='/about' component={Routes.AboutUs} />
-            <ProtectedRoute path='/wallet' component={Routes.Wallet} />
-          </div>
-        </Router>
+              <Route exact path='/' component={Routes.Home} />
+              <Route exact path='/profile' component={Routes.Profile} />
+              <Route exact path='/login' component={Routes.Login} />
+              <Route path='/about' component={Routes.AboutUs} />
+              <ProtectedRoute path='/wallet' component={Routes.Wallet} />
+            </div>
+          </Router>
+        </div>
       </MuiThemeProvider>)
   }
 }
@@ -61,7 +87,7 @@ class App extends React.Component {
 const ProtectedLink = ({to, children}) => {
   if (!isAuthenticated()) return null
 
-  return <li><Link to={to}>{children}</Link></li>
+  return <MenuItem onTouchTap={() => AppStore.menuIsOpen = false}><Link to={to}>{children}</Link></MenuItem>
 }
 
 const ProtectedRoute = ({component: Component, ...rest}) => (
