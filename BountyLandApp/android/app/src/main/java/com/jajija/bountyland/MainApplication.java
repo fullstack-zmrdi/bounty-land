30428fdf4a5a8b64a35d794a13a9a119c2fa9d45
Jakub Martyčák <martycakjakub@gmail.com>
2017-04-07 15:42:23 +0200

    Fix android build

---
+++ BountyLandApp/android/app/src/main/java/com/jajija/bountyland/MainApplication.java
@@ -0,0 +1,43 @@
+package com.jajija.bountyland;
+
+import android.content.Context;
+import android.support.multidex.MultiDex;
+
+import com.jajija.bountyland.BuildConfig;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.remobile.toast.RCTToastPackage;
+
+import co.apptailor.googlesignin.RNGoogleSigninPackage;
+import com.magus.fblogin.FacebookLoginPackage;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
+import com.facebook.react.ReactPackage;
+import com.reactnativenavigation.NavigationApplication;
+
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends NavigationApplication {
+  @Override
+  protected void attachBaseContext(Context context) {
+    super.attachBaseContext(context);
+    MultiDex.install(this);
+  }
+
+  @Override
+  public boolean isDebug() {
+    // Make sure you are using BuildConfig from your own application
+    return BuildConfig.DEBUG;
+  }
+
+  @Override
+  public List<ReactPackage> createAdditionalReactPackages() {
+    return Arrays.<ReactPackage>asList(
+          new VectorIconsPackage(),
+          new RCTToastPackage(),
+          new RNGoogleSigninPackage(),
+          new FacebookLoginPackage(),
+          new RCTCameraPackage()
+    );
+  }
+}
---
+++ BountyLandApp/app/const.js
@@ -0,0 +1,8 @@
+export const firebaseConfig ={
+    apiKey: "AIzaSyC1FEVDK4GvldU-Ztm8PRcudbYJfn_CU5I",
+    authDomain: "bounttylandapp.firebaseapp.com",
+    databaseURL: "https://bounttylandapp.firebaseio.com",
+    projectId: "bounttylandapp",
+    storageBucket: "bounttylandapp.appspot.com",
+    messagingSenderId: "21402723903"
+}
