9d5cd24f7610159b7164ac7cd64b40a4ca05a32a
Jiri Spac <capajj@gmail.com>
2017-04-07 23:40:58 +0200

    wallet has tabs

--- web/src/routes.js
+++ web/src/routes.js
@@ -1,46 +1,35 @@
-// import React from 'react'
-// import Layout from './layout'
-// import { browserHistory, createMemoryHistory, Router, Route, IndexRoute } from 'react-router'
-// import LandingPage from './routes/landing-page'
-// import AboutUs from './routes/about-us'
-//
-// export const history = typeof document !== 'undefined' ? browserHistory : createMemoryHistory()
-//
-// export const router = <Router history={history}>
-//   <Route path='/' component={Layout}>
-//     <IndexRoute component={LandingPage} />
-//     <Route path='o-nas' component={AboutUs} />
-//   </Route>
-// </Router>
-//
-// export default () => router
 import React from 'react'
 import {
   BrowserRouter as Router,
   Route,
   Link
 } from 'react-router-dom'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme'
+import getMuiTheme from 'material-ui/styles/getMuiTheme'
 import AboutUs from './routes/about-us'
 import Wallet from './routes/wallet'
 
 const App = () => (
-  <Router>
-    <div>
-      <ul>
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/topics">Topics</Link></li>
-        <li><Link to="/wallet">Wallet</Link></li>
-      </ul>
+  <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+    <Router>
+      <div>
+        <ul>
+          <li><Link to='/'>Home</Link></li>
+          <li><Link to='/about'>About</Link></li>
+          <li><Link to='/topics'>Topics</Link></li>
+          <li><Link to='/wallet'>Wallet</Link></li>
+        </ul>
 
-      <hr/>
+        <hr />
 
-      <Route exact path="/" component={Home}/>
-      <Route path="/about" component={AboutUs}/>
-      <Route path="/topics" component={Topics}/>
-      <Route path="/wallet" component={Wallet}/>
-    </div>
-  </Router>
+        <Route exact path='/' component={Home} />
+        <Route path='/about' component={AboutUs} />
+        <Route path='/topics' component={Topics} />
+        <Route path='/wallet' component={Wallet} />
+      </div>
+    </Router>
+  </MuiThemeProvider>
 )
 
 const Home = () => (
--- web/src/routes/wallet.js
+++ web/src/routes/wallet.js
@@ -1,13 +1,51 @@
-import React, { Component } from 'react'
+import React from 'react'
+import {Tabs, Tab} from 'material-ui/Tabs'
+import Slider from 'material-ui/Slider'
+import {observer} from 'mobx-react'
+import {observable} from 'mobx'
 
-class Wallet extends Component {
-  render () {
-    return (
-      <div>
-        wallet
-      </div>
-    )
+const styles = {
+  headline: {
+    fontSize: 24,
+    paddingTop: 16,
+    marginBottom: 12,
+    fontWeight: 400
   }
 }
+
+const state = observable({
+  tab: 'top up'
+})
+
+const Wallet = observer(() => (
+  <Tabs value={state.tab} onChange={(tab) => {
+    state.tab = tab
+  }}>
+    <Tab label='transactions' value='transactions' onClick={() => {
+      state.tab = 'transactions'
+    }}>
+      <div>
+        <h2 style={styles.headline}>Tab One</h2>
+        <p>
+          This sis an example tab.
+        </p>
+        <p>
+          You can put any sort of HTML or react component in here. It even keeps the component state!
+        </p>
+        <Slider name='slider0' defaultValue={0.5} />
+      </div>
+    </Tab>
+    <Tab label='top up' value='top up' onClick={() => {
+      state.tab = 'top up'
+    }}>
+      <div>
+        <h2 style={styles.headline}>Tab Two</h2>
+        <p>
+          This is another example tab.
+        </p>
+      </div>
+    </Tab>
+  </Tabs>
+))
 
 export default Wallet
