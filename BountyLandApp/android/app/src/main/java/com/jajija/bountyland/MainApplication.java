149ffb286d0521c7e6edb3f70cd03022f998e75c
Jakub Martyčák <martycakjakub@gmail.com>
2017-04-15 16:16:25 +0200

    [mobile] Replace react-native-facebook-login with react-native-fbsdk, update fb frameworks ios

--- BountyLandApp/android/app/src/main/java/com/jajija/bountyland/MainApplication.java
+++ BountyLandApp/android/app/src/main/java/com/jajija/bountyland/MainApplication.java
@@ -1,43 +1,69 @@
 package com.jajija.bountyland;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.multidex.MultiDex;
 
-import com.jajija.bountyland.BuildConfig;
-import com.oblador.vectoricons.VectorIconsPackage;
-
 import co.apptailor.googlesignin.RNGoogleSigninPackage;
-import com.magus.fblogin.FacebookLoginPackage;
-import com.lwansbrough.RCTCamera.RCTCameraPackage;
-import com.facebook.react.ReactPackage;
-import com.reactnativenavigation.NavigationApplication;
+
 import com.airbnb.android.react.maps.MapsPackage;
+import com.jajija.bountyland.BuildConfig;
+import com.facebook.appevents.AppEventsLogger;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookSdk;
+import com.facebook.react.ReactPackage;
+import com.facebook.reactnative.androidsdk.FBSDKPackage;
+import com.lwansbrough.RCTCamera.RCTCameraPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.reactnativenavigation.NavigationApplication;
+import com.reactnativenavigation.controllers.ActivityCallbacks;
+import com.remobile.toast.RCTToastPackage;
 
 import java.util.Arrays;
 import java.util.List;
 
 public class MainApplication extends NavigationApplication {
-  @Override
-  protected void attachBaseContext(Context context) {
-    super.attachBaseContext(context);
-    MultiDex.install(this);
-  }
 
-  @Override
-  public boolean isDebug() {
-    // Make sure you are using BuildConfig from your own application
-    return BuildConfig.DEBUG;
-  }
+    private static CallbackManager mCallbackManager = CallbackManager.Factory.create();
 
-  @Override
-  public List<ReactPackage> createAdditionalReactPackages() {
-    return Arrays.<ReactPackage>asList(
-          new VectorIconsPackage(),
-          new RCTToastPackage(),
-          new RNGoogleSigninPackage(),
-          new FacebookLoginPackage(),
-          new RCTCameraPackage(),
+    protected static CallbackManager getCallbackManager() {
+        return mCallbackManager;
+    }
+
+    @Override
+    protected void attachBaseContext(Context context) {
+        super.attachBaseContext(context);
+        MultiDex.install(this);
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        AppEventsLogger.activateApp(this); // If you want to use AppEventsLogger to log events.
+        setActivityCallbacks(new ActivityCallbacks() {
+            @Override
+            public void onActivityResult(int requestCode, int resultCode, Intent data) {
+                super.onActivityResult(requestCode, resultCode, data);
+                getCallbackManager().onActivityResult(requestCode, resultCode, data);
+            }
+        });
+    }
+
+
+    @Override
+    public boolean isDebug() {
+        return BuildConfig.DEBUG; // Make sure you are using BuildConfig from your own application
+    }
+
+    @Override
+    public List<ReactPackage> createAdditionalReactPackages() {
+        return Arrays.<ReactPackage>asList(
+            new VectorIconsPackage(),
+            new RCTToastPackage(),
+            new RNGoogleSigninPackage(),
+            new FBSDKPackage(mCallbackManager),
             new MapsPackage()
-    );
-  }
+        );
+    }
 }
--- BountyLandApp/ios/BountyLandApp/AppDelegate.m
+++ BountyLandApp/ios/BountyLandApp/AppDelegate.m
@@ -1,18 +1,11 @@
-#import "AppDelegate.h"
 #import <React/RCTBundleURLProvider.h>
-
-// **********************************************
-// *** DON'T MISS: THE NEXT LINE IS IMPORTANT ***
-// **********************************************
-#import "RCCManager.h"
-
-// IMPORTANT: if you're getting an Xcode error that RCCManager.h isn't found, you've probably ran "npm install"
-// with npm ver 2. You'll need to "npm install" with npm 3 (see https://github.com/wix/react-native-navigation/issues/1)
-
 #import <React/RCTRootView.h>
 #import <FBSDKCoreKit/FBSDKCoreKit.h>
 #import <FBSDKLoginKit/FBSDKLoginKit.h>
 #import <RNGoogleSignin/RNGoogleSignin.h>
+
+#import "AppDelegate.h"
+#import "RCCManager.h"
 
 @implementation AppDelegate
 
@@ -21,7 +14,7 @@
   NSURL *jsCodeLocation;
 
 #ifdef DEBUG
-  // jsCodeLocation = [NSURL URLWithString:@"http://localhost:8081/index.ios.bundle?platform=ios&dev=true"];
+  // jsCodeLocation = [NSURL URLWithString:@"http://192.168.0.24:8081/index.ios.bundle?platform=ios&dev=true"];
   jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index.ios" fallbackResource:nil];
 #else
   jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
